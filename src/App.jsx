68152f29251ea99f147d3aeb7aa1f8f20c732ba5
Mon0205 <nhuy02042003@gmail.com>
2025-12-09 16:30:31 +0700

    update footer

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,7 @@ import Register from "./page/Register";
 import ForgetPassword from "./page/ForgetPassword";
 import ProfilePage from "./page/ProfilePage";
 import CartPage from "./page/CartPage";
+import "./App.css";
 function App() {
   return (
     // Đây là nơi Router "bọc" toàn bộ ứng dụng
